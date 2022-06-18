defcdddc73c69d3b8f6a77be399d5ffdc91316fb
herisusantoarismann <heri.s.arisman@gmail.com>
2022-06-18 13:38:40 +0700

    Add layout and category

---
+++ utils/category.js
@@ -0,0 +1,54 @@
+const category = [
+  {
+    name: "Arts & Literature",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Film & TV",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Food & Drink",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "General Knowledge",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Geography",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "History",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Music",
+    query: "Science",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Science",
+    query: "Science",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Society & Culture",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+  {
+    name: "Sport & Leisure",
+    query: "arts_and_literature",
+    logo: "<ImagesearchRollerIcon />",
+  },
+];
+
+export { category };
