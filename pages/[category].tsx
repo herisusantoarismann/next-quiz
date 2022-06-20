b8fe6d9aaad87a8c9d068dd7b38e6bc67016be9f
herisusantoarismann <heri.s.arisman@gmail.com>
2022-06-20 12:58:18 +0700

    Add fetching data

--- pages/[category].tsx
+++ pages/[category].tsx
@@ -1,13 +1,23 @@
 import React, { FunctionComponent } from "react";
 
-interface Props {}
+interface Props {
+  data: string;
+}
 
-const Category: FunctionComponent<Props> = () => {
+const Category: FunctionComponent<Props> = ({ data }) => {
   return (
     <div>
       <h1>Category</h1>
     </div>
   );
 };
+
+export async function getServerSideProps(context: any) {
+  const { category } = context.query;
+  const res = await fetch(`https://restcountries.eu/rest/v2/name/${category}`);
+  const data = await res.json();
+
+  return { props: { data } };
+}
 
 export default Category;
